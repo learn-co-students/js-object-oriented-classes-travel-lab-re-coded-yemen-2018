6344b92829910a28c282eb1103e3bdd7ca967f08
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-28 18:19:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
 
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  };
+  yearsExperienceFromBeginningOf(year){
+    return  year - this.startDate.getFullYear();
+  };
+};
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
+  blocksTravelled(){
+    let horizontal = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
+    let vertical = this.endingLocation.vertical - this.beginningLocation.vertical;
+    return horizontal + vertical;
+  };
+  estimatedTime(peakOrNot){
+    if(peakOrNot){
+      return this.blocksTravelled() / 2
+    }
+    return this.blocksTravelled() / 3
+  };
+};
